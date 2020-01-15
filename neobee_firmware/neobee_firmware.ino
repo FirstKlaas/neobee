7ca406c20179ea313aab46820f52951868aa96c8
FirstKlaas <klaas.nebuhr@gmail.com>
2020-01-15 13:35:54 +0100

    Created an empty sketch for the neobee firmware.

---
+++ neobee_firmware/neobee_firmware.ino
@@ -0,0 +1,21 @@
+/****************************************
+ * neobee
+ * 
+ * Reading sensor data from a hive and 
+ * push it to an mqtt broker. 
+ * 
+ * For more information about this project
+ * check out the github page:
+ * https://github.com/FirstKlaas/neobee
+ *
+ * Author: FirstKlaas <klaas.nebuhr@gmail.com>
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
