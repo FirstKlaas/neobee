64e0fc61f9c63d41c4d22635ffcdaf694e395d9c
FirstKlaas <klaas.nebuhr@gmail.com>
2020-01-16 14:51:55 +0100

    Added the ds18b20 dependency and some setup code.

--- neobee_firmware/neobee_firmware.ino
+++ neobee_firmware/neobee_firmware.ino
@@ -12,12 +12,22 @@
  */
 
 #include "HX711.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define SERIAL_SPEED 115200
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
+
+// The pin, the data wire of the Dallas sensor is connected to.
+#define ONE_WIRE_BUS 2
+#define TEMPERATURE_PRECISION 9
+
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
 
 // DeepSleep time in microseconds
 // If you set the number to 0, you need to trigger the
@@ -32,6 +42,12 @@ const float CALIBRATION_FACTOR = 2280.f;
 
 // The HX711 scale instance
 HX711 scale;
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device addresses
+DeviceAddress insideThermometer, outsideThermometer;
 
 #define DEBUG
  
@@ -48,6 +64,22 @@ void setup() {
   // By omitting the gain factor parameter, the library
   // default "128" (Channel A) is used here.
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  // Start the ds18b20 sensors to work
+  sensors.begin();
+
+
+  // Get the adresses for the dallas sensors
+  if (!sensors.getAddress(insideThermometer, 0)) {
+    #ifdef DEBUG
+      Serial.println("Unable to find address for Device 0");
+    #endif
+  }
+  if (!sensors.getAddress(outsideThermometer, 1)) {
+    #ifdef DEBUG
+      Serial.println("Unable to find address for Device 1");
+    #endif
+  }
 }
 
 void loop() {
