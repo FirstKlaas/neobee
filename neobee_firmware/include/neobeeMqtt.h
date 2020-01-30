b4d6cf799fb7c8cecc47295e324bc01f21db0ee6
FirstKlaas <klaas.nebuhr@gmail.com>
2020-01-30 17:06:17 +0100

    Updated Drawing

--- neobee_firmware/include/neobeeMqtt.h
+++ neobee_firmware/include/neobeeMqtt.h
@@ -13,9 +13,30 @@ class NeoBeeMqtt
     ~NeoBeeMqtt();
 
     void sendMessage();
-    void setMqttHost(const uint8_t* host);
+    void setHost(const uint8_t* host);
+
+    /**
+     * Prints the mqtt host to dest.
+     * It just copies the bytes from the configuration
+     * context to the destination. Size is the size of
+     * the dest buffer. The number of bytes copied is
+     * the minimum of the size parameter and the length
+     * of the mqtt host in the configuration context.
+     * Returns true, if the full mqtt hostname could be 
+     * copied, false else.
+     **/  
+    bool printHost(uint8_t dest, uint8_t size);
+    
+    void setPort(uint16_t port);
+
+    uint16_t getPort(); 
 
     uint8_t operator[](const uint8_t index);
+
+    /**
+     * Returns the buffer size for the message
+     * to be broadcasted.
+     **/
     size_t bufferSize() const;
 
   private:
