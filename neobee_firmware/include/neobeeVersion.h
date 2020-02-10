f71cb789ba19603bc62a6cd9bd4f15942860fdb5
FirstKlaas <klaas.nebuhr@gmail.com>
2020-02-10 20:34:16 +0100

    Copied all firmware files (automated)

--- neobee_firmware/include/neobeeVersion.h
+++ neobee_firmware/include/neobeeVersion.h
@@ -1,7 +1,7 @@
 /**********************************
  Auto generated file. Do not edit.
 
- Generated at 2020-02-10T17:24
+ Generated at 2020-02-10T20:25
 **********************************/
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
--- neobee_firmware/prepare.py
+++ neobee_firmware/prepare.py
@@ -25,6 +25,8 @@ def copy_firmware():
     version_string = ".".join([format(x) for x in version])
     copyfile(".pio/build/wemos_debug/firmware.bin", "firmware/neobee_wemos_latest-debug.bin")
     copyfile(".pio/build/wemos_debug/firmware.bin", f"firmware/neobee_wemos_{version_string}-debug.bin")
+    copyfile(".pio/build/wemos_release/firmware.bin", "firmware/neobee_wemos_latest.bin")
+    copyfile(".pio/build/wemos_release/firmware.bin", f"firmware/neobee_wemos_{version_string}.bin")
 
 if __name__ == "__main__":
     generate_version_header()
