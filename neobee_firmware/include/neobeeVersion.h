793711fff4cb468a6741352ace49af068b131df4
firstklaas <klaas.nebuhr@gmail.com>
2021-03-29 22:03:57 +0200

    Version o.1.8

--- neobee_firmware/include/neobeeVersion.h
+++ neobee_firmware/include/neobeeVersion.h
@@ -1,8 +1,8 @@
 /**********************************
  Auto generated file. Do not edit.
 
- Generated at 2021-03-13T14:48
+ Generated at 2021-03-29T22:01
 **********************************/
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
-#define BUILD_VERSION 7
+#define BUILD_VERSION 8
